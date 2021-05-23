e91865e2d670a2df7216926f818f01efbdb5d9f1
Boris Tane <boris.tane@gmail.com>
2021-05-23 06:24:41 +0100

    search internal

---
+++ src/controllers/search-internal.ts
@@ -0,0 +1,44 @@
+import database from "../services/database";
+import logger from "logger";
+import { Context, APIGatewayEvent } from "aws-lambda";
+import algolia from "../services/algolia";
+import { failure, handleError, IHTTPResponse, success } from "../utils/http-responses";
+import { wrapper } from "../utils/controllers-helpers";
+import { IBookmark } from "../models/bookmark";
+import { IUser } from "../models/user";
+
+async function search(event: APIGatewayEvent): Promise<IHTTPResponse> {
+  try {
+    const body = JSON.parse(event.body!);
+    const { userId, query } = body;
+    if (!query) {
+      return failure({ message: "Bad Request" }, 400);
+    }
+
+    const user = await database.getOwner(userId, false) as IUser;
+
+    const promises = user.organisations!.map(async organisation => {
+      return await algolia.search(organisation, query);
+    }).flat();
+    const hits = (await Promise.all(promises)).flat();
+
+
+    const data = {
+      message: "Got search results",
+      data: {
+        user: userId,
+        hits,
+      },
+    };
+
+    return success(data);
+  } catch (err) {
+    const message = "Unexpected error when getting the search results internal";
+    return handleError(err, event.body, message);
+  }
+}
+
+export async function handler(event: APIGatewayEvent, context: Context) {
+  const correlationId = event.headers["x-correlation-id"];
+  return await logger.bindFunction(wrapper, correlationId)(search, event);
+}
