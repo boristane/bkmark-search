0c7e23ea3f54fbfffe4755892f6271e421a710d0
Boris Tane <boris.tane@gmail.com>
2021-06-04 18:20:26 +0100

    fix more issues with the migration

--- src/migrations/2021-06-03_08:11:00.ts
+++ src/migrations/2021-06-03_08:11:00.ts
@@ -14,6 +14,8 @@ import algolia from "../services/algolia";
 import database from "../services/database";
 import database2 from "../services/database2";
 import fs from "fs";
+import { initialiseDb } from "../utils/db-helpers";
+import { DocumentClient } from "aws-sdk/clients/dynamodb";
 
 async function addSortKeysToAllMfs() {
 
@@ -46,9 +48,9 @@ async function addSortKeysToAllMfs() {
   await Promise.all(organisationsPromises);
   await Promise.all(usersPromises);
 
-  for(let i = 0; i < objectIds.length; i += 1) {
+  for (let i = 0; i < objectIds.length; i += 1) {
     const bookmark = objectIds[i];
-    if(bookmark.organisationId.length > 12) continue;
+    if (bookmark.organisationId.length > 12) continue;
     try {
       logger.info("Processing bookmark", { organisationId: bookmark.organisationId, objectId: bookmark.objectId });
       const algoliaObject = await algolia.getBookmark(bookmark.organisationId, bookmark.objectId);
@@ -64,4 +66,83 @@ async function addSortKeysToAllMfs() {
   fs.writeFileSync("failures.json", JSON.stringify(failures));
 }
 
+async function handleTheOnesWhichDontHaveAType() {
+  const all = await scanAll();
+  const failures: any[] = [];
+
+  const badApples = all.filter(item => !item.type);
+  const objectIds = badApples.filter(apple => apple.partitionKey.includes("bookmark")).map(item => {
+    return {
+      objectId: item.data.objectId,
+      organisationId: item.partitionKey.split("#")[1],
+      uuid: item.partitionKey.split("#")[3],
+    }
+  });
+  const users = badApples.filter(apple => !apple.partitionKey.includes("bookmark")).map(u => u.data);
+
+  const usersPromises = users.map(async user => {
+    try {
+      logger.info("Processing user", { userId: user.uuid });
+      await database2.createOwner(user, false);
+    } catch (error) {
+      logger.error("There was a problem with a user", { error, user });
+      failures.push({ ...user, type: "user" })
+    }
+  });
+
+  await Promise.all(usersPromises);
+
+  for (let i = 0; i < objectIds.length; i += 1) {
+    const bookmark = objectIds[i];
+    if (bookmark.organisationId.length > 12) continue;
+    try {
+      logger.info("Processing bookmark", { organisationId: bookmark.organisationId, objectId: bookmark.objectId });
+      const algoliaObject = await algolia.getBookmark(bookmark.organisationId, bookmark.objectId);
+      await database2.createBookmark(bookmark.objectId, algoliaObject);
+    } catch (error) {
+      logger.error("There was a problem with a bookmark", { error, bookmark });
+      failures.push({ ...bookmark, type: "bookmark" });
+    }
+  }
+
+  fs.writeFileSync("bad-apples.json", JSON.stringify(badApples));
+  fs.writeFileSync("users.json", JSON.stringify(users));
+  fs.writeFileSync("bookmarks.json", JSON.stringify(objectIds));
+
+  logger.error("Here are the failures", failures);
+  fs.writeFileSync("failures.json", JSON.stringify(failures));
+}
+
+async function scanAll() {
+
+  const { tableName, dynamoDb } = initialiseDb(process.env.PROJECTION_TABLE!);
+  let lastEvaluatedKey: DocumentClient.Key | undefined = undefined;
+  let items: any[] = [];
+  do {
+    const params = {
+      TableName: tableName,
+      ExclusiveStartKey: lastEvaluatedKey,
+    };
+
+    try {
+      const records: DocumentClient.ScanOutput = await dynamoDb.scan(params).promise();
+      if (!records.Items) {
+        return [];
+      }
+      const result = records.Items;
+      items = [...items, ...result];
+      lastEvaluatedKey = records.LastEvaluatedKey;
+    } catch (error) {
+      logger.error("There was a problem getting all the items from dynamoDB", {
+        error,
+      });
+      throw error;
+    }
+  } while (lastEvaluatedKey !== undefined);
+  return items;
+}
+
+
+handleTheOnesWhichDontHaveAType();
 addSortKeysToAllMfs();
+
